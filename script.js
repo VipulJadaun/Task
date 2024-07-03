97e006488c84249b480862627f65e0a708074343
Varun <varunshrivastav36@gmail.com>
2024-07-03 14:34:28 +0530

    main

--- script.js
+++ script.js
@@ -1,41 +1,47 @@
-var login = document.querySelector(".navbar .right #b2")
-var loginPage = document.querySelector(".login")
-var close = document.querySelector(".login .cards i")
-var close1 = document.querySelector(".signup .cards i")
+const wheelUrl = "https://dapper-daffodil-2c7d31.netlify.app/";
+var login = document.querySelector(".navbar .right #b2");
+var loginPage = document.querySelector(".login");
+var close = document.querySelector(".login .cards i");
+var close1 = document.querySelector(".signup .cards i");
+var openWheelButton = document.getElementById("f1");
+
+function openNewTab(url) {
+  window.open(url, "_blank");
+}
+
+openWheelButton.addEventListener("click", () => {
+  openNewTab(wheelUrl);
+});
 
 close.addEventListener("click", function () {
-    loginPage.style.display = 'none'
-})
+  loginPage.style.display = "none";
+});
 login.addEventListener("click", function () {
-    loginPage.style.display = 'flex'
-
-})
-
+  loginPage.style.display = "flex";
+});
 
 close1.addEventListener("click", function () {
-    signUpPage.style.display = 'none'
-})
-var signUp = document.querySelector(".navbar .right #b1")
-var signUpPage = document.querySelector(".signup")
+  signUpPage.style.display = "none";
+});
+var signUp = document.querySelector(".navbar .right #b1");
+var signUpPage = document.querySelector(".signup");
 
 signUp.addEventListener("click", function () {
-    signUpPage.style.display = 'flex'
-})
-
-
+  signUpPage.style.display = "flex";
+});
 
 // var navbar = document.querySelector("#navbar")
 // var right = document.querySelector(".right p")
-var menu = document.querySelector(".navbar #menu")
-var loader = document.querySelector(".loader")
+var menu = document.querySelector(".navbar #menu");
+var loader = document.querySelector(".loader");
 function toggleLoader() {
-    if (loader.style.top === "0%") {
-        loader.style.top = "-100%";
-        console.log("Loader hidden");
-    } else {
-        loader.style.top = "0%";
-        console.log("Loader shown");
-    }
+  if (loader.style.top === "0%") {
+    loader.style.top = "-100%";
+    console.log("Loader hidden");
+  } else {
+    loader.style.top = "0%";
+    console.log("Loader shown");
+  }
 }
 
 menu.addEventListener("click", toggleLoader);
